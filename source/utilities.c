5d1ae6c1ef5db14582abd803ad8042d02d2eabcc
Jon Torgeir Grini <71172809+torgeirgrini@users.noreply.github.com>
2021-02-22 15:16:31 +0100

    Update utilities.c

--- source/utilities.c
+++ source/utilities.c
@@ -1,8 +1,17 @@
 #include "utilities.h"
+#include "hardware.h"
 
 void control_timer(int milliseconds) {
     int timer_finished = 0;
     while(!timer_finished) {
         timer_finished = setTimer(milliseconds);
+    }
+}
+
+int read_all_floor_sensors() {
+    for(int i = 0; i < HARDWARE_NUMBER_OF_FLOORS; i++){
+                    if(hardware_read_floor_sensor(i)) {
+                        return i;
+                    }
     }
 }
