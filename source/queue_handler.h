be398aadf48f313cc27d93949603a93bafd907c4
Jon Torgeir Grini <torgeirgrini@gmail.com>
2021-03-11 12:22:34 +0100

    doxygen queue

--- source/queue_handler.h
+++ source/queue_handler.h
@@ -1,3 +1,7 @@
+/**
+* @file
+* @brief A library for doing operations with a queue matrix defined in an Elevator struct
+*/ 
 #ifndef QUEUE_HANDLER_H
 #define QUEUE_HANDLER_H
 
@@ -6,26 +10,67 @@
 
 #define QUEUE_HANDLER_NUMBER_OF_ROWS 3
 
-//Sets all elements of queue_matrix to zero.
+/**
+ * @brief Sets all elements of queue_matrix in an Elevator struct to zero.
+ * 
+ * @param[out] p_elev Pointer to an Elevator struct
+ *
+ */
 void queue_clear(Elevator *p_elev);
 
-//Returns 1 if there is and order at queue_matrix[order_type][floor]. 0 otherwise
+/**
+ * @brief Check a specified value in queue_matrix in an Elevator struct
+ * 
+ * @param[in] p_elev Pointer to an Elevator struct
+ * @param[in] order_type The order type to check
+ * @param[in] floor The floor to check
+ * 
+ * @return 1 if there is an unhandled order at @p floor with order type @p order_type in @p p_elev, 0 otherwise 
+*/
 int queue_get_order(Elevator *p_elev, HardwareOrder order_type, int floor);
 
-//Updates queue_matrix with new orders
+/**
+ * @brief Updates queue_matrix in an Elevator struct with new orders
+
+ * @param[out] p_elev Pointer to an Elevator struct 
+ */
 void queue_set_orders(Elevator *p_elev);
 
-//Deletes the order at location queue_matrix[order_type][floor]
+/**
+ * @brief Sets a specified value in queue_matrix in an Elevator struct to zero
+ 
+ * @param[out] p_elev Pointer to an Elevator struct
+ * @param[in] order_type The order type to nullify
+ * @param[in] floor The floor to nullify
+ */
 void queue_delete_order(Elevator *p_elev, HardwareOrder order_type, int floor);
 
-//Returns 1 if there are any untreated orders at the floor, 0 otherwise
+/**
+ * @brief Checks a floor for any unhandled orders
+ * 
+ * @param[in] p_elev Pointer to an Elevator struct
+ * @param[in] floor The floor to check
+ * 
+ * @returns 1 if there are any unhandled orders at the @p floor, 0 otherwise
+ */
 int queue_active_orders_floor(Elevator *p_elev, int floor);
 
-//Iterates through all elements of queue_matrix to see if any orders are active. Returns 0 if there are active orders in 
-//the upwards direction, 1 to indicate stop, 2 to indicate active orders in downwards direction
+/**
+ * @brief Checks if there are any unhandled orders in an Elevator struct
+ * 
+ * @param[out] p_elev pointer to an Elevator struct
+ * 
+ * @return 1 if there are any unhandled orders in queue_matrix in @p p_elev
+ */
 int queue_active_orders_all_floors(Elevator *p_elev);
 
-//Looks for any order, returns movement direction of elevator
+/**
+ * @brief Checks for the direction for the elevator to move independent of priority
+ * 
+ * @param[in] p_elev pointer to an Elevator struct
+ * 
+ * @return the direction for the elevator that @p p_elev represents to move
+ */
 HardwareMovement queue_get_movement_any_direction(Elevator *p_elev);
 
 //Returns direction of the next order to be treated
