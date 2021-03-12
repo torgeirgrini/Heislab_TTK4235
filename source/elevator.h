fb0dc5b6a21f6c216b00b96591df1a9488ad054a
kristian.hope <kristian.hope@propulsentnu.no>
2021-03-12 16:19:44 +0100

    doxy elevator

--- source/elevator.h
+++ source/elevator.h
@@ -44,17 +44,34 @@ typedef struct Elevator {
 
 } Elevator;
 
-/*
-Initializes the queue matrix to zero matrix. Current_floor to the current floor [0,4), or -1 if in the shaft. Current_dir to 1.
-Current_state to INIT.
-*/
+/**
+ * @brief Function that initializes an Elevator struct. 
+ * The queue matrix is set to zero matrix, current_floor to the current floor [0,4) or -1 if in the shaft, 
+ * current_movement, last_dir and current_order_dir to HARDWARE_MOVEMENT_STOP, 
+ * and current_state to IDLE_IN_FLOOR.
+ * @param[out] p_elev Pointer to an Elevator struct
+ */
 void elevator_init(Elevator *p_elev);
 
-//on startup takes the elevator to idle in floor
+
+/**
+ * @brief Function that makes the elevator ready to be operated. If the elevator is in the shaft, 
+ * it will move upwards until it arrives at a floor. 
+ * @param[out] p_elev Pointer to an Elevator struct
+ */
 void elevator_startup_routine(Elevator *p_elev);
 
+/**
+ * @brief Function that sets the order lights corresponding to an order.
+ * @param[out] p_elev Pointer to an Elevator struct
+ */
 void elevator_order_light_on(Elevator *p_elev);
 
+
+/**
+ * @brief Function that sets current_floor and last_floor in @p p_elev depending on which floor the elevator the fllor is in
+ * @param[out] p_elev Pointer to an Elevator struct
+ */
 void elevator_update_floor(Elevator *p_elev);
 
 
