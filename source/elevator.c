e69a4844794266626153a57bcf819e71e8c13b93
kristian.hope <kristian.hope@propulsentnu.no>
2021-03-12 16:41:21 +0100

    changed init

--- source/elevator.c
+++ source/elevator.c
@@ -4,7 +4,7 @@ void elevator_init(Elevator *p_elev) {
     p_elev->current_floor = -1;
     p_elev->previous_floor = -1;
     p_elev->current_movement = HARDWARE_MOVEMENT_STOP;
-    p_elev->current_state = IDLE_IN_FLOOR;
+    p_elev->current_state = INIT;
     p_elev->stop_light_set = 0;
     p_elev->timer_set = 0;
     p_elev->order_direction = HARDWARE_MOVEMENT_STOP;
@@ -25,6 +25,7 @@ void elevator_startup_routine(Elevator *p_elev) {
     hardware_command_movement(HARDWARE_MOVEMENT_STOP);
     p_elev->previous_direction = HARDWARE_MOVEMENT_UP;
     p_elev->current_movement = HARDWARE_MOVEMENT_STOP;
+    p_elev->current_state = IDLE_IN_FLOOR;
     elevator_update_floor(p_elev);
 }
 
--- source/states.h
+++ source/states.h
@@ -1,8 +1,17 @@
+/**
+* @file
+* @brief Header file containing the state enum
+*/
+
 #ifndef STATES_H
 #define STATES_H
 
+/**
+ * @enum Enum containing all 
+ */
+
 typedef enum state {
-    IDLE_IN_FLOOR, IDLE_IN_SHAFT, MOVEMENT, DOOR_OPEN, TIMER, STOP_BTN_SHAFT, STOP_BTN_FLOOR
+    INIT, IDLE_IN_FLOOR, IDLE_IN_SHAFT, MOVEMENT, DOOR_OPEN, TIMER, STOP_BTN_SHAFT, STOP_BTN_FLOOR
 } state;
 
 #endif
