834eb77915d69ba20cc494915bb170020cb859da
kristian.hope <kristian.hope@propulsentnu.no>
2021-03-14 16:23:54 +0100

    doxy fix

--- source/states.h
+++ source/states.h
@@ -1,20 +1,20 @@
 /**
 * @file
-* @brief Library containing the state enum
+* @brief Enum containing the states of the elevator
 */
 
 #ifndef STATES_H
 #define STATES_H
 
 /**
- * @enum Enum containing all states
+ * @enum state Enum
  */
 
 typedef enum state {
     INIT, /**<State for initialization*/
     IDLE_IN_FLOOR, /**<Elevator is idle in a floor*/
     IDLE_IN_SHAFT, /**<Elevator is idle in the shaft*/
-    MOVEMENT, /**<Elevator is moving between floors*/
+    MOVEMENT, /**<Elevator is moving*/
     DOOR_OPEN, /**<Intermediate state for opening the door*/
     TIMER, /**<Door timer is active*/
     STOP_BTN_SHAFT, /**<Stop button has been pressed when elevator is in the shaft*/
